e63d1bd3b6cb4b9b94a39467b570da8fef4c8fc1
Federico Mendez <fedemendez2014@hotmail.com>
2018-03-20 18:18:26 -0300

    Va commit con el ejercicio del lunes 20

--- app/src/main/java/inicio/myapplication/MainActivity.java
+++ app/src/main/java/inicio/myapplication/MainActivity.java
@@ -2,12 +2,25 @@ package inicio.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView txtNom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.button);
+        txtNom = (TextView)findViewById(R.id.text);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtNom.setText("Federico Mendez");
+            }
+        });
     }
 }
